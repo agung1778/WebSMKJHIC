adde6080add762f59db4a81b5e03d03a6f25a83d
agung1778 <foxs8841@gmail.com>
2026-09-11 06:48:25 +0700

    perbaiki 405 saat simpan data: paksa skema https untuk URL yang di-generate di hosting (form action http -> 301 -> GET)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::anonymousComponentNamespace('admin.components', 'admin-components');
+
+        // Hosting di belakang proxy/cPanel sering terdeteksi sebagai HTTP,
+        // sehingga route()/url() memakai http:// dan POST form diarahkan 301 -> GET -> 405.
+        // Paksa HTTPS untuk domain asli; biarkan localhost/127.0.0.1 memakai http.
+        $host = request()->getHost();
+        if (! in_array($host, ['127.0.0.1', 'localhost'], true)) {
+            URL::forceScheme('https');
+        }
     }
 }
